8b1038b8682a1473d0e0053d50c36ee833645e02
Turgay <124166016+turgaykirdar@users.noreply.github.com>
2024-10-05 00:32:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+VANTA.BIRDS({
+   el: "#vanta",
+   mouseControls: true,
+   touchControls: true,
+   gyroControls: true,
+   minHeight: 600.00,
+   minWeight: 200.00,
+   scale: 1.00,
+   scaleMobile: 1.00,
+   quantity: 2.00, // Animasyon sayısı
+   backgroundAlpha: 0.0,
+   alignment: 77.00,
+});
